43915cd5872936c79e6154cdeffcb257a267dcf2
zhixing <>
2019-08-03 11:28:05 +0800

    dd

---
+++ Localizable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localizable
+//
+//  Created by zhixing on 2019/8/3.
+//  Copyright © 2019 zhixing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
